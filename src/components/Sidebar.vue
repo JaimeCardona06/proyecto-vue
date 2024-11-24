6b58158f655b0f5a5ef50c7468adc6210154c012
JaimeCardona06 <jaime.cardona40549@ucaldas.edu.co>
2024-11-24 14:40:58 -0500

    Instala vee-validate. Crear modulos.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,8 @@
         <h2>Barra lateral</h2>
         <ul>
             <li><router-link to="/contador">Contador</router-link></li>
-            <li><router-link to="lista-de-tareas">Lista de Tareas</router-link></li>
+            <li><router-link to="/lista-de-tareas">Lista de Tareas</router-link></li>
+            <li><router-link to="/registrar">Registrar</router-link></li>
         </ul>
     </div>
 </template>
--- src/modules/listaDeTareas/components/ListaDeTareas.vue
+++ src/modules/listaDeTareas/components/ListaDeTareas.vue
@@ -3,16 +3,16 @@
         <h2>Lista de Tareas</h2>
         <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea()">
         <ul>
-            <li v-for="(tarea, index) in tareas" :key="index">
+            <li v-for="(tarea, index) in tareas" :key="index" >
                 {{ tarea }}
-                <button @click="removeTarea(index)">Eliminar Tarea</button>
+                <button @click="removeTarea(index)">Eliminar</button>
             </li>
         </ul>
     </div>
 </template>
 
 <script setup>
-    import {ref} from 'vue';
+    import { ref } from 'vue';
 
     const tareas = ref([]);
     const nuevaTarea = ref('');
@@ -20,13 +20,11 @@
         tareas.value.push(nuevaTarea.value);
         nuevaTarea.value = '';
     }
-    
-    const removeTarea = (index) =>{
+
+    const removeTarea = (index) => {
         tareas.value.splice(index, 1);
     }
-
 </script>
 
 <style scoped>
-
 </style>
