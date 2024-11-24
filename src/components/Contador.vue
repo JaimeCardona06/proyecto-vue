9ad0daa6dfc6cc78ca6f8199d9c41f0946ef79c2
JaimeCardona06 <jaime.cardona40549@ucaldas.edu.co>
2024-11-24 12:55:29 -0500

    Crea componente de contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Hola mundo</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar()">Incrementar</button>
+  <button @click="decrementar()">Decrementar</button>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+
+  const contador = ref(0);
+
+  const incrementar = () => {
+    //.value sirve para obtener o cambiar el valor de una variable reactiva
+    contador.value++;
+  }
+
+  const decrementar = () =>{
+    contador.value--;
+  }
+
+</script>
+  
+<style scoped>
+  p{
+    color: red;
+  }
+</style>
